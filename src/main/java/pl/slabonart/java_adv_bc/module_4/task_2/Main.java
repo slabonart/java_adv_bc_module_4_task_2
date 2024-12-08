2c37b205baa024ce141c97470299879212ff533c
Artur_Slabon <artur_slabon@epam.com>
2024-12-08 13:41:13 +0100

    Initial commit

---
+++ src/main/java/pl/slabonart/java_adv_bc/module_4/task_2/Main.java
@@ -0,0 +1,7 @@
+package pl.slabonart.java_adv_bc.module_2.task_5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/pl/slabonart/java_adv_bc/module_4/task_2/UserController.java
@@ -0,0 +1,4 @@
+package pl.slabonart.java_adv_bc.module_2.task_5;
+
+public class UserController {
+}
